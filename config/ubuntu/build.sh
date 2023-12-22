c6b175e9cab461975f16ec281b815ced5457333b
David Hadas <david.hadas@gmail.com>
2023-12-22 17:31:41 +0200

    improved example Dockerfile

--- config/ubuntu/build.sh
+++ config/ubuntu/build.sh
@@ -1,3 +1,5 @@
-GOARCH="amd64" GOOS=linux go build ../../cmd/seal-wrap/wrap.go
-docker build . -t quay.io/dhadas/wrap-ubuntu --platform linux/amd64
+#GOARCH="amd64" GOOS=linux go build ../../cmd/seal-wrap/wrap.go
+#docker build . -t quay.io/dhadas/wrap-ubuntu --platform linux/amd64
+cp ~/.kube/config config/ubuntu/kubeconfig
+docker build config/ubuntu/. -t quay.io/dhadas/wrap-ubuntu --build-arg workloadname=test
 docker push quay.io/dhadas/wrap-ubuntu
